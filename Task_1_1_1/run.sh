0107b198b4c0b3459a271570feee43b5fc67cc53
msansdudu <msansdudu@mail.ru>
2024-09-12 19:14:44 +0700

    first fixing style

--- Task_1_1_1/run.sh
+++ Task_1_1_1/run.sh
@@ -1,4 +1,4 @@
-javac src/main/java/nsu_chebotareva/HeapSort.java -d ./build
-javadoc -d build/docs/javadoc -sourcepath src/main/java -subpackages nsu_chebotareva
-jar -cfm HeapSort.jar META-INF/MANIFEST.MF src/main/java/nsu_chebotareva/HeapSort.class -d ./build
-java -cp ./build nsu_chebotareva.HeapSort
+javac src/main/java/nsu.chebotareva/HeapSort.java -d ./build
+javadoc -d build/docs/javadoc -sourcepath src/main/java -subpackages nsu.chebotareva
+jar -cfm HeapSort.jar META-INF/MANIFEST.MF src/main/java/nsu.chebotareva/HeapSort.class -d ./build
+java -cp ./build nsu.chebotareva.HeapSort
