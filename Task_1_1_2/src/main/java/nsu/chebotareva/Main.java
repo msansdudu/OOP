1d8b463ed1f5314fd4bfa93269841957bb11cb72
msansdudu <msansdudu@mail.ru>
2024-09-17 23:15:24 +0700

    making rounds, score and dealers playing

--- Task_1_1_2/src/main/java/nsu/chebotareva/Main.java
+++ Task_1_1_2/src/main/java/nsu/chebotareva/Main.java
@@ -3,25 +3,75 @@ package nsu.chebotareva;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static int round (int numberOfRound) {
         Person player = new Person();
         Person dealer = new Person();
-        Boolean isOpenDealer = Boolean.FALSE; // карта дилера пока закрыта
 
         Cards.dealingCards (player, 2);
         Cards.dealingCards (dealer, 2);
-        System.out.println("Добро пожаловать в Блэкджек!\nРаунд 1\nДилер раздал карты");
-        player.printingCards(Boolean.FALSE, isOpenDealer);
-        dealer.printingCards(Boolean.TRUE, isOpenDealer);
+        System.out.printf("Раунд %d%nДилер раздал карты%n", numberOfRound);
+        player.printingCards(Boolean.FALSE, Boolean.FALSE);
+        dealer.printingCards(Boolean.TRUE, Boolean.FALSE);
         // раздали начальные карты
+        if (player.sumOfCosts == 21) {
+            return 1;
+        }
 
         System.out.println("\nВаш ход\n-------\nВведите “1”, чтобы взять карту, и “0”, чтобы остановиться...");
         Scanner sc = new Scanner(System.in);
         while (sc.nextInt() == 1) {
             player.newCardForPlayer(dealer);
+            if (player.sumOfCosts == 21) {
+                sc.close();
+                return 1;
+            }
+            if (player.sumOfCosts > 21) {
+                sc.close();
+                return 0;
+            }
             System.out.println("\nВаш ход\n-------\nВведите “1”, чтобы взять карту, и “0”, чтобы остановиться...");
         }
         // игрок набрал карты
+
+        System.out.printf("%nХод дилера%n-------%nДилер открывает закрытую карту %s (%d)%n", dealer.cards[1], dealer.costs[1]);
+        player.printingCards(Boolean.FALSE, Boolean.TRUE);
+        dealer.printingCards(Boolean.TRUE, Boolean.TRUE);
+        while (dealer.sumOfCosts < 17) {
+            dealer.newCardForDealer(player);
+            if (dealer.sumOfCosts > 21) {
+                sc.close();
+                return 1;
+            }
+        }
+        if (player.sumOfCosts > dealer.sumOfCosts) {
+            sc.close();
+            return 1;
+        }
+        if (player.sumOfCosts == dealer.sumOfCosts) {
+            sc.close();
+            return 3;
+        }
+        sc.close();
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в Блэкджек!");
+        int plScore = 0, dlScore = 0;
+        int win;
+        win = round(1);
+        if (win == 1) {
+            plScore++;
+            System.out.printf("%nВы выиграли раунд! Счет %d:%d%s.%n", plScore, dlScore, plScore > dlScore ? " в вашу пользу" : (plScore == dlScore) ? ". Счет сравнялся!" : " в пользу дилера");
+        }
+        else if (win == 0) {
+            dlScore++;
+            System.out.printf("%nВы проиграли раунд! Счет %d:%d%s.%n", plScore, dlScore, plScore > dlScore ? " в вашу пользу" : (plScore == dlScore) ? ". Счет сравнялся!" : " в пользу дилера");
+        }
+        else {
+            System.out.printf("Ничья! Счет %d:%d%s.%n", plScore, dlScore, plScore > dlScore ? " в вашу пользу" : (plScore == dlScore) ? ". Счет сравнялся!" : " в пользу дилера");
+        }
+        System.out.println("Желаете продолжить игру? Введите “1”, чтобы продолжить, и “0”, чтобы прекратить игру.");
 
 
     }
--- Task_1_1_2/src/main/java/nsu/chebotareva/Person.java
+++ Task_1_1_2/src/main/java/nsu/chebotareva/Person.java
@@ -8,20 +8,20 @@ public class Person {
 
     public void printingCards (Boolean isDealer, Boolean isOpenDealer) {
         if (!isDealer) {
-            System.out.print("Ваши карты: [");
+            System.out.print("\tВаши карты: [");
             for (int i = 0; i < amountOfCards - 1; i++){
                 System.out.printf("%s (%d), ", cards[i], costs[i]);
             }
             System.out.printf("%s (%d)] => %d%n", cards[amountOfCards - 1], costs[amountOfCards - 1], sumOfCosts);
         } else {
             if (isOpenDealer) {
-                System.out.print("Карты дилера: [");
+                System.out.print("\tКарты дилера: [");
                 for (int i = 0; i < amountOfCards - 1; i++){
                     System.out.printf("%s (%d), ", cards[i], costs[i]);
                 }
                 System.out.printf("%s (%d)] => %d%n", cards[amountOfCards - 1], costs[amountOfCards - 1], sumOfCosts);
             } else {
-                System.out.printf("Карты дилера: [%s (%d), <закрытая карта>]%n", cards[0], costs[0]);
+                System.out.printf("\tКарты дилера: [%s (%d), <закрытая карта>]%n", cards[0], costs[0]);
             }
         }
     }
@@ -31,5 +31,12 @@ public class Person {
         System.out.printf("Вы открыли карту %s (%d)%n", this.cards[this.amountOfCards - 1], this.costs[this.amountOfCards - 1]);
         printingCards(Boolean.FALSE, Boolean.FALSE);
         dealer.printingCards(Boolean.TRUE, Boolean.FALSE);
+    }
+
+    public void newCardForDealer (Person player) {
+        Cards.dealingCards(this, 1);
+        System.out.printf("%nДилер открывает карту %s (%d)%n", this.cards[this.amountOfCards - 1], this.costs[this.amountOfCards - 1]);
+        player.printingCards(Boolean.FALSE, Boolean.FALSE);
+        printingCards(Boolean.TRUE, Boolean.TRUE);
     }
 }
