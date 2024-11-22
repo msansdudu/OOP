571bd6d54c41cbf988d8d3af1271d997886b2d21
msansdudu <msansdudu@mail.ru>
2024-11-22 15:51:45 +0700

    upd

--- Task_1_3_1/src/main/java/nsu/chebotareva/Main.java
+++ Task_1_3_1/src/main/java/nsu/chebotareva/Main.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String fileName = "input.txt";
-        String target = "nn";
-        List<Long> result = SubstringFinder.findSubstringIndex(fileName, target);
-        System.out.println("Индексы вхождений подстроки: " + result);
+    public static void main(String[] args) {
     }
 }
--- Task_1_3_1/src/main/java/nsu/chebotareva/SubstringFinder.java
+++ Task_1_3_1/src/main/java/nsu/chebotareva/SubstringFinder.java
@@ -1,42 +1,73 @@
 package nsu.chebotareva;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SubstringFinder {
     public static List<Long> findSubstringIndex(String fileName, String substr) throws IOException {
+        BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
         List<Long> index = new ArrayList<>();
-        byte[] substrBytes = substr.getBytes(StandardCharsets.UTF_8);
-        int substrLength = substrBytes.length;
+        int substrLen = substr.length();
+        int bufferSize = substrLen * 2;
+        char[] buffer = new char[bufferSize];
+        int amountOfOccurences = 0;
+        int amountOfSymbols = 0;
 
-        try (RandomAccessFile file = new RandomAccessFile(fileName, "r")) {
-            long fileLength = file.length();
-            byte[] buff = new byte[substrLength * 2];
+        long pos = 0;
+        int readChar;
+        int EOF = substrLen * 2 + 1;
 
-            long pos = 0;
-            while (pos < fileLength) {
-                int bytesRead = file.read(buff, 0, Math.min(buff.length, (int) (fileLength - pos)));
+        for (int i = 0; i < substrLen; i++) {
+            readChar = file.read();
+            amountOfSymbols++;
+            if (readChar == -1) {
+                EOF = i + substrLen;
+                break;
+            }
+            buffer[i] = (char) readChar;
+        }
 
-                if (bytesRead < substrLength) {
+        while (EOF > substrLen) {
+            for (int i = 0; i < substrLen; i++) {
+                readChar = file.read();
+                amountOfSymbols++;
+                if (readChar == -1) {
+                    EOF = i;
                     break;
                 }
-                for (int i = 0; i <= bytesRead - substrLength; i++) {
-                    if (isMatch(buff, substrBytes, i)) {
+                buffer[i + substrLen] = (char) readChar;
+            }
+            for (int i = 0; i <= bufferSize - substrLen; i++) {
+                if (isMatch(buffer, substr, i, substrLen, EOF)) {
+                    if (amountOfOccurences == 0 || index.get(amountOfOccurences - 1) != pos + i) {
                         index.add(pos + i);
+                        amountOfOccurences++;
                     }
                 }
-                pos += bytesRead - substrLength + 1;
-                file.seek(pos);
             }
+
+            pos += bufferSize - substrLen;
+
+            for (int i = 0; i < substrLen; i++) {
+                buffer[i] = buffer[i + substrLen];
+            }
+        }
+        file.close();
+        amountOfSymbols--;
+        if (amountOfOccurences != 0 && index.get(amountOfOccurences - 1) == amountOfSymbols) {
+            index.remove(amountOfOccurences - 1);
         }
         return index;
     }
 
-    private static boolean isMatch(byte[] buff, byte[] substrBytes, int beg) {
-        for (int i = 0; i < substrBytes.length; i++) {
-            if (buff[beg + i] != substrBytes[i]) {
+    private static boolean isMatch(char[] buff, String substr, int beg, int substrLen, int EOF) {
+        for (int i = 0; i < substrLen; i++) {
+            if (buff[beg + i] != substr.charAt(i)) {
                 return false;
             }
         }
